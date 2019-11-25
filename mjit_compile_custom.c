830d4f9c149bc6cdec372bc20a4f79ad21cbc201
Takashi Kokubun <takashikkbn@gmail.com>
2019-11-24 23:24:17 -0800

    Use C local for Ruby locals k, canonical

--- mjit_compile_custom.c
+++ mjit_compile_custom.c
@@ -2,12 +2,13 @@ bool
 mjit_compile_custom(FILE *f, const rb_iseq_t *iseq, struct compile_status *status)
 {
     VALUE* opes = iseq->body->iseq_encoded;
+    fprintf(f, "    VALUE k = *(vm_get_ep(GET_EP(), 0) - 0x5);\n");
     fprintf(f, "    VALUE stack_0, stack_1, stack_2;\n");
     fprintf(f, "    static const rb_iseq_t *original_iseq = (const rb_iseq_t *)0x%"PRIxVALUE";\n", (VALUE)iseq);
     fprintf(f, "    static const VALUE *const original_body_iseq = (VALUE *)0x%"PRIxVALUE";\n\n", (VALUE)iseq->body->iseq_encoded);
 
     // label_0: getlocal_WC_0 - k
-    fprintf(f, "    stack_0 = *(vm_get_ep(GET_EP(), 0) - 0x5);\n");
+    fprintf(f, "    stack_0 = k;\n");
 
     // label_2: opt_send_without_block - #downcase
     fprintf(f, "    reg_cfp->pc = original_body_iseq + 4;\n");
@@ -28,14 +29,14 @@ mjit_compile_custom(FILE *f, const rb_iseq_t *iseq, struct compile_status *statu
     fprintf(f, "    }\n");
 
     // label_6: setlocal - canonical
-    fprintf(f, "    vm_env_write(vm_get_ep(GET_EP(), 0), -(int)0x3, stack_0);\n");
+    fprintf(f, "    VALUE canonical = stack_0;\n");
 
     // label_8: getinstancevariable - @names
     fprintf(f, "    reg_cfp->pc = original_body_iseq + 11;\n");
     fprintf(f, "    stack_0 = vm_getinstancevariable(GET_SELF(), (ID)0x%"PRIxVALUE", (IC)0x%"PRIxVALUE");\n", opes[9], opes[10]);
 
     // label_11: getlocal_WC_0 - canonical
-    fprintf(f, "    stack_1 = *(vm_get_ep(GET_EP(), 0) - 0x3);\n");
+    fprintf(f, "    stack_1 = canonical;\n");
 
     // label_13: opt_aref
     fprintf(f, "    reg_cfp->pc = original_body_iseq + 15;\n");
@@ -58,7 +59,7 @@ mjit_compile_custom(FILE *f, const rb_iseq_t *iseq, struct compile_status *statu
     fprintf(f, "    stack_0 = vm_getinstancevariable(GET_SELF(), (ID)0x%"PRIxVALUE", (IC)0x%"PRIxVALUE");\n", opes[18], opes[19]);
 
     // label_20: getlocal_WC_0 - canonical
-    fprintf(f, "    stack_1 = *(vm_get_ep(GET_EP(), 0) - 0x3);\n");
+    fprintf(f, "    stack_1 = canonical;\n");
 
     // label_22: opt_aref
     fprintf(f, "    reg_cfp->pc = original_body_iseq + 24;\n");
@@ -71,7 +72,7 @@ mjit_compile_custom(FILE *f, const rb_iseq_t *iseq, struct compile_status *statu
     fprintf(f, "    stack_0 = val;\n");
 
     // label_24: getlocal_WC_0 - k
-    fprintf(f, "    stack_1 = *(vm_get_ep(GET_EP(), 0) - 0x5);\n");
+    fprintf(f, "    stack_1 = k;\n");
 
     // label_26: opt_neq
     fprintf(f, "    stack_0 = vm_opt_neq((CALL_DATA)0x%"PRIxVALUE", (CALL_DATA)0x%"PRIxVALUE", stack_0, stack_1);\n", opes[28], opes[27]);
@@ -92,7 +93,7 @@ mjit_compile_custom(FILE *f, const rb_iseq_t *iseq, struct compile_status *statu
     fprintf(f, "    stack_0 = GET_SELF();\n");
 
     // label_32: getlocal_WC_0 - k
-    fprintf(f, "    stack_1 = *(vm_get_ep(GET_EP(), 0) - 0x5);\n");
+    fprintf(f, "    stack_1 = k;\n");
 
     // label_34: opt_send_without_block
     fprintf(f, "    reg_cfp->pc = original_body_iseq + 36;\n");
@@ -109,10 +110,10 @@ mjit_compile_custom(FILE *f, const rb_iseq_t *iseq, struct compile_status *statu
     fprintf(f, "    stack_0 = vm_getinstancevariable(GET_SELF(), (ID)0x%"PRIxVALUE", (IC)0x%"PRIxVALUE");\n", opes[38], opes[39]);
 
     // label_40: getlocal_WC_0 - canonical
-    fprintf(f, "    stack_1 = *(vm_get_ep(GET_EP(), 0) - 0x3);\n");
+    fprintf(f, "    stack_1 = canonical;\n");
 
     // label_42: getlocal_WC_0 - k
-    fprintf(f, "    stack_2 = *(vm_get_ep(GET_EP(), 0) - 0x5);\n");
+    fprintf(f, "    stack_2 = k;\n");
 
     // label_44: opt_aset
     fprintf(f, "    reg_cfp->pc = original_body_iseq + 46;\n");
@@ -127,7 +128,7 @@ mjit_compile_custom(FILE *f, const rb_iseq_t *iseq, struct compile_status *statu
     fprintf(f, "    stack_0 = GET_SELF();\n");
 
     // label_48: getlocal_WC_0 - k
-    fprintf(f, "    stack_1 = *(vm_get_ep(GET_EP(), 0) - 0x5);\n");
+    fprintf(f, "    stack_1 = k;\n");
 
     // label_50: getlocal_WC_0 - v
     fprintf(f, "    stack_2 = *(vm_get_ep(GET_EP(), 0) - 0x4);\n");
