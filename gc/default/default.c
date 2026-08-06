k0kubun/ruby

commit 583668d5ed12331fd1b1361d6cad3f2378ddda2b

Remove gc_bump_pointer_heap
It was leftover and we don't actually need it.

gc/default/default.c
@@ -241,15 +241,17 @@ static RB_THREAD_LOCAL_SPECIFIER int malloc_increase_local;
 #endif
 
 typedef struct ractor_newobj_heap_cache {
+    uintptr_t cursor;
+    uintptr_t cursor_end;
     struct free_region *next_region;
     struct heap_page *using_page;
     uintptr_t region_end;
+    size_t allocated_objects_count;
 } rb_ractor_newobj_heap_cache_t;
 
 typedef struct ractor_newobj_cache {
     size_t incremental_mark_step_allocated_slots;
     rb_ractor_newobj_heap_cache_t heap_caches[HEAP_COUNT];
-    struct gc_bump_pointer_heap bump_heaps[];
 } rb_ractor_newobj_cache_t;
 
 typedef struct {
@@ -1764,31 +1766,11 @@ calloc1(size_t n)
     return calloc(1, n);
 }
 
-static void
-gc_ractor_jit_cursor_sync(void *c, void *data)
-{
-    rb_ractor_newobj_cache_t *gc_cache = c;
-    bool tracing = (bool)(uintptr_t)data;
-
-    for (size_t heap_idx = 0; heap_idx < HEAP_COUNT; heap_idx++) {
-        struct gc_bump_pointer_heap *bump = &gc_cache->bump_heaps[heap_idx];
-        bump->jit_cursor_end = tracing ? bump->cursor : bump->cursor_end;
-    }
-}
-
 void
 rb_gc_impl_set_event_hook(void *objspace_ptr, const rb_event_flag_t event)
 {
     rb_objspace_t *objspace = objspace_ptr;
-    rb_event_flag_t old_events = objspace->hook_events;
     objspace->hook_events = event & RUBY_INTERNAL_EVENT_OBJSPACE_MASK;
-
-    bool was_tracing = old_events & RUBY_INTERNAL_EVENT_NEWOBJ;
-    bool now_tracing = objspace->hook_events & RUBY_INTERNAL_EVENT_NEWOBJ;
-    if (was_tracing != now_tracing) {
-        rb_gc_ractor_newobj_cache_foreach(gc_ractor_jit_cursor_sync,
-                                          (void *)(uintptr_t)now_tracing);
-    }
 }
 
 unsigned long long
@@ -2498,14 +2480,11 @@ rb_gc_impl_size_allocatable_p(size_t size)
 }
 
 static inline void
-gc_bump_flush_alloc_count(struct gc_bump_pointer_heap *bump, rb_heap_t *heap)
+gc_bump_flush_alloc_count(rb_ractor_newobj_heap_cache_t *heap_cache, rb_heap_t *heap)
 {
-    if (bump->slot_size == 0) return;
-
-    size_t n = (bump->cursor - bump->region_start) / bump->slot_size;
-    if (n > 0) {
-        RUBY_ATOMIC_SIZE_ADD(heap->total_allocated_objects, n);
-        bump->region_start = bump->cursor;
+    if (heap_cache->allocated_objects_count > 0) {
+        RUBY_ATOMIC_SIZE_ADD(heap->total_allocated_objects, heap_cache->allocated_objects_count);
+        heap_cache->allocated_objects_count = 0;
     }
 }
 
@@ -2513,31 +2492,29 @@ static void
 ractor_cache_flush_count(rb_objspace_t *objspace, rb_ractor_newobj_cache_t *gc_cache)
 {
     for (int heap_idx = 0; heap_idx < HEAP_COUNT; heap_idx++) {
-        gc_bump_flush_alloc_count(&gc_cache->bump_heaps[heap_idx], &heaps[heap_idx]);
+        gc_bump_flush_alloc_count(&gc_cache->heap_caches[heap_idx], &heaps[heap_idx]);
     }
 }
 
 static inline void
-ractor_cache_open_window(rb_objspace_t *objspace, struct gc_bump_pointer_heap *bump,
-                         rb_ractor_newobj_heap_cache_t *heap_cache)
+ractor_cache_open_window(rb_objspace_t *objspace, rb_ractor_newobj_heap_cache_t *heap_cache,
+                         size_t heap_idx)
 {
     uintptr_t end = heap_cache->region_end;
 
     if (RB_UNLIKELY(is_incremental_marking(objspace))) {
-        uintptr_t window_end = bump->cursor + INCREMENTAL_MARK_STEP_ALLOCATIONS * bump->slot_size;
+        uintptr_t window_end = heap_cache->cursor + INCREMENTAL_MARK_STEP_ALLOCATIONS * pool_slot_sizes[heap_idx];
         if (window_end < end) end = window_end;
     }
 
-    bump->cursor_end = end;
-    bump->jit_cursor_end =
-        RB_UNLIKELY(objspace->hook_events & RUBY_INTERNAL_EVENT_NEWOBJ) ? bump->cursor : end;
+    heap_cache->cursor_end = end;
 }
 
 static inline bool
-ractor_cache_advance_region(rb_objspace_t *objspace, struct gc_bump_pointer_heap *bump,
-                            rb_ractor_newobj_heap_cache_t *heap_cache, rb_heap_t *heap)
+ractor_cache_advance_region(rb_objspace_t *objspace, rb_ractor_newobj_heap_cache_t *heap_cache,
+                            size_t heap_idx)
 {
-    gc_bump_flush_alloc_count(bump, heap);
+    gc_bump_flush_alloc_count(heap_cache, &heaps[heap_idx]);
 
     struct free_region *region = heap_cache->next_region;
     if (region == NULL) {
@@ -2546,13 +2523,12 @@ ractor_cache_advance_region(rb_objspace_t *objspace, struct gc_bump_pointer_heap
 
     rb_asan_unpoison_object((VALUE)region, false);
     GC_ASSERT(RB_TYPE_P((VALUE)region, T_NONE));
-    bump->cursor = (uintptr_t)region;
-    bump->region_start = (uintptr_t)region;
+    heap_cache->cursor = (uintptr_t)region;
     heap_cache->region_end = region->end;
     heap_cache->next_region = region->next;
     rb_asan_poison_object((VALUE)region);
 
-    ractor_cache_open_window(objspace, bump, heap_cache);
+    ractor_cache_open_window(objspace, heap_cache, heap_idx);
 
     return true;
 }
@@ -2561,24 +2537,25 @@ static inline VALUE
 ractor_cache_allocate_slot(rb_objspace_t *objspace, rb_ractor_newobj_cache_t *gc_cache,
                            size_t heap_idx)
 {
-    struct gc_bump_pointer_heap *bump = &gc_cache->bump_heaps[heap_idx];
+    rb_ractor_newobj_heap_cache_t *heap_cache = &gc_cache->heap_caches[heap_idx];
+    size_t slot_size = pool_slot_sizes[heap_idx];
 
-    uintptr_t cursor = bump->cursor;
-    if (RB_UNLIKELY(cursor + bump->slot_size > bump->cursor_end)) {
+    uintptr_t cursor = heap_cache->cursor;
+    if (RB_UNLIKELY(cursor + slot_size > heap_cache->cursor_end)) {
         if (RB_UNLIKELY(is_incremental_marking(objspace))) {
             return Qfalse;
         }
 
-        rb_ractor_newobj_heap_cache_t *heap_cache = &gc_cache->heap_caches[heap_idx];
-        if (!ractor_cache_advance_region(objspace, bump, heap_cache, &heaps[heap_idx])) {
+        if (!ractor_cache_advance_region(objspace, heap_cache, heap_idx)) {
             return Qfalse;
         }
-        cursor = bump->cursor;
+        cursor = heap_cache->cursor;
     }
 
     VALUE obj = (VALUE)cursor;
     rb_asan_unpoison_object(obj, true);
-    bump->cursor = cursor + bump->slot_size;
+    heap_cache->cursor = cursor + slot_size;
+    heap_cache->allocated_objects_count++;
 
 #if RGENGC_CHECK_MODE
     GC_ASSERT(rb_gc_impl_obj_slot_size(obj) == heap_slot_size(heap_idx));
@@ -2612,10 +2589,9 @@ ractor_cache_set_page(rb_objspace_t *objspace, rb_ractor_newobj_cache_t *gc_cach
 {
     gc_report(3, objspace, "ractor_set_cache: Using page %p\n", (void *)page->body);
 
-    struct gc_bump_pointer_heap *bump = &gc_cache->bump_heaps[heap_idx];
     rb_ractor_newobj_heap_cache_t *heap_cache = &gc_cache->heap_caches[heap_idx];
 
-    GC_ASSERT(bump->cursor + bump->slot_size > bump->cursor_end);
+    GC_ASSERT(heap_cache->cursor + pool_slot_sizes[heap_idx] > heap_cache->cursor_end);
     GC_ASSERT(heap_cache->next_region == NULL);
     GC_ASSERT(page->free_slots != 0);
     GC_ASSERT(page->free_region != NULL);
@@ -2625,13 +2601,12 @@ ractor_cache_set_page(rb_objspace_t *objspace, rb_ractor_newobj_cache_t *gc_cach
     struct free_region *region = page->free_region;
     rb_asan_unpoison_object((VALUE)region, false);
     GC_ASSERT(RB_TYPE_P((VALUE)region, T_NONE));
-    bump->cursor = (uintptr_t)region;
-    bump->region_start = (uintptr_t)region;
+    heap_cache->cursor = (uintptr_t)region;
     heap_cache->region_end = region->end;
     heap_cache->next_region = region->next;
     rb_asan_poison_object((VALUE)region);
 
-    ractor_cache_open_window(objspace, bump, heap_cache);
+    ractor_cache_open_window(objspace, heap_cache, heap_idx);
 
     page->free_slots = 0;
     page->free_region = NULL;
@@ -2684,12 +2659,12 @@ rb_gc_impl_zjit_new_obj_fastpath(void *objspace_ptr, size_t alloc_size, VALUE fl
     }
     if (slot_size == 0) return false;
 
-    size_t bump_base = offsetof(rb_ractor_newobj_cache_t, bump_heaps) +
-                       heap_idx * sizeof(struct gc_bump_pointer_heap);
+    size_t base = offsetof(rb_ractor_newobj_cache_t, heap_caches) +
+                  heap_idx * sizeof(rb_ractor_newobj_heap_cache_t);
 
     struct rb_gc_zjit_default_new_obj_fastpath default_fastpath = {
-        bump_base + offsetof(struct gc_bump_pointer_heap, cursor),
-        bump_base + offsetof(struct gc_bump_pointer_heap, jit_cursor_end),
+        base + offsetof(rb_ractor_newobj_heap_cache_t, cursor),
+        base + offsetof(rb_ractor_newobj_heap_cache_t, cursor_end),
         slot_size,
         flags,
         klass
@@ -2710,7 +2685,6 @@ NOINLINE(static VALUE newobj_bump_pointer_miss(rb_objspace_t *objspace, rb_racto
 static VALUE
 newobj_bump_pointer_miss(rb_objspace_t *objspace, rb_ractor_newobj_cache_t *gc_cache, size_t heap_idx, bool vm_locked)
 {
-    struct gc_bump_pointer_heap *bump = &gc_cache->bump_heaps[heap_idx];
     rb_ractor_newobj_cache_t *cache = gc_cache;
     rb_ractor_newobj_heap_cache_t *heap_cache = &cache->heap_caches[heap_idx];
     rb_heap_t *heap = &heaps[heap_idx];
@@ -2743,25 +2717,22 @@ newobj_bump_pointer_miss(rb_objspace_t *objspace, rb_ractor_newobj_cache_t *gc_c
         }
 
         if (is_incremental_marking(objspace)) {
-            if (bump->slot_size > 0) {
-                cache->incremental_mark_step_allocated_slots +=
-                    (bump->cursor - bump->region_start) / bump->slot_size;
-            }
-            gc_bump_flush_alloc_count(bump, heap);
+            cache->incremental_mark_step_allocated_slots += heap_cache->allocated_objects_count;
+            gc_bump_flush_alloc_count(heap_cache, heap);
 
             if (cache->incremental_mark_step_allocated_slots >= INCREMENTAL_MARK_STEP_ALLOCATIONS) {
                 gc_continue(objspace, heap);
                 cache->incremental_mark_step_allocated_slots = 0;
             }
 
-            if (bump->cursor + bump->slot_size <= heap_cache->region_end) {
-                ractor_cache_open_window(objspace, bump, heap_cache);
+            if (heap_cache->cursor + pool_slot_sizes[heap_idx] <= heap_cache->region_end) {
+                ractor_cache_open_window(objspace, heap_cache, heap_idx);
                 obj = ractor_cache_allocate_slot(objspace, gc_cache, heap_idx);
             }
         }
 
         if (obj == Qfalse) {
-            if (ractor_cache_advance_region(objspace, bump, heap_cache, heap)) {
+            if (ractor_cache_advance_region(objspace, heap_cache, heap_idx)) {
                 obj = ractor_cache_allocate_slot(objspace, gc_cache, heap_idx);
             }
         }
@@ -2774,8 +2745,7 @@ newobj_bump_pointer_miss(rb_objspace_t *objspace, rb_ractor_newobj_cache_t *gc_c
         }
 
         if (RB_UNLIKELY(ruby_gc_stressful)) {
-            bump->cursor_end = bump->cursor;
-            bump->jit_cursor_end = bump->cursor;
+            heap_cache->cursor_end = heap_cache->cursor;
         }
     }
 
@@ -4112,13 +4082,12 @@ gc_mode_transition(rb_objspace_t *objspace, enum gc_mode mode)
 }
 
 static void
-heap_page_flush_cache_regions(struct heap_page *page, struct gc_bump_pointer_heap *bump,
-                              rb_ractor_newobj_heap_cache_t *heap_cache)
+heap_page_flush_cache_regions(struct heap_page *page, rb_ractor_newobj_heap_cache_t *heap_cache)
 {
     struct free_region *chain = heap_cache->next_region;
 
-    if (bump->cursor < heap_cache->region_end) {
-        VALUE start = (VALUE)bump->cursor;
+    if (heap_cache->cursor < heap_cache->region_end) {
+        VALUE start = (VALUE)heap_cache->cursor;
         rb_asan_unpoison_object(start, false);
         struct free_region *remnant = (struct free_region *)start;
         remnant->flags = 0;
@@ -4186,26 +4155,23 @@ gc_ractor_newobj_cache_clear(void *c, void *data)
     newobj_cache->incremental_mark_step_allocated_slots = 0;
 
     for (size_t heap_idx = 0; heap_idx < HEAP_COUNT; heap_idx++) {
-        struct gc_bump_pointer_heap *bump = &gc_cache->bump_heaps[heap_idx];
         rb_ractor_newobj_heap_cache_t *cache = &newobj_cache->heap_caches[heap_idx];
 
         rb_heap_t *heap = &heaps[heap_idx];
-        gc_bump_flush_alloc_count(bump, heap);
+        gc_bump_flush_alloc_count(cache, heap);
 
         struct heap_page *page = cache->using_page;
-        RUBY_DEBUG_LOG("ractor using_page:%p cursor:%p", (void *)page, (void *)bump->cursor);
+        RUBY_DEBUG_LOG("ractor using_page:%p cursor:%p", (void *)page, (void *)cache->cursor);
 
         if (page) {
-            heap_page_flush_cache_regions(page, bump, cache);
+            heap_page_flush_cache_regions(page, cache);
         }
 
         cache->using_page = NULL;
         cache->next_region = NULL;
         cache->region_end = 0;
-        bump->cursor = 0;
-        bump->cursor_end = 0;
-        bump->jit_cursor_end = 0;
-        bump->region_start = 0;
+        cache->cursor = 0;
+        cache->cursor_end = 0;
     }
 }
 
@@ -4215,9 +4181,8 @@ gc_ractor_newobj_cache_exhaust(void *c, void *data)
     rb_ractor_newobj_cache_t *gc_cache = c;
 
     for (size_t heap_idx = 0; heap_idx < HEAP_COUNT; heap_idx++) {
-        struct gc_bump_pointer_heap *bump = &gc_cache->bump_heaps[heap_idx];
-        bump->cursor_end = bump->cursor;
-        bump->jit_cursor_end = bump->cursor;
+        rb_ractor_newobj_heap_cache_t *heap_cache = &gc_cache->heap_caches[heap_idx];
+        heap_cache->cursor_end = heap_cache->cursor;
     }
 }
 
@@ -6826,13 +6791,8 @@ rb_gc_impl_ractor_cache_alloc(void *objspace_ptr, void *ractor)
 
     objspace->live_ractor_cache_count++;
 
-    rb_ractor_newobj_cache_t *gc_cache =
-        calloc1(sizeof(rb_ractor_newobj_cache_t) +
-                sizeof(struct gc_bump_pointer_heap) * (HEAP_COUNT + 1));
+    rb_ractor_newobj_cache_t *gc_cache = calloc1(sizeof(rb_ractor_newobj_cache_t));
 
-    for (size_t i = 0; i < HEAP_COUNT; i++) {
-        gc_cache->bump_heaps[i].slot_size = pool_slot_sizes[i];
-    }
     return gc_cache;
 }
 

gc/default/zjit_fastpath.h
@@ -9,7 +9,7 @@
 
 struct rb_gc_zjit_default_new_obj_fastpath {
     size_t cursor_offset;
-    size_t jit_cursor_end_offset;
+    size_t cursor_end_offset;
     size_t slot_size;
     VALUE flags;
     VALUE klass;

zjit/src/codegen/gc_fastpath.rs
@@ -12,7 +12,7 @@ use super::JITState;
 #[derive(Clone, Copy)]
 struct RbGcZjitDefaultNewObjFastpath {
     cursor_offset: usize,
-    jit_cursor_end_offset: usize,
+    cursor_end_offset: usize,
     slot_size: usize,
     flags: VALUE,
     klass: VALUE,
@@ -173,7 +173,7 @@ fn emit_default_new_obj_fastpath(
     miss: &Target,
 ) -> Option<Opnd> {
     let cursor_offset: i32 = fastpath.cursor_offset.try_into().ok()?;
-    let jit_cursor_end_offset: i32 = fastpath.jit_cursor_end_offset.try_into().ok()?;
+    let cursor_end_offset: i32 = fastpath.cursor_end_offset.try_into().ok()?;
     let slot_size: u64 = fastpath.slot_size.try_into().ok()?;
 
     let thread = asm.load(Opnd::mem(64, EC, RUBY_OFFSET_EC_THREAD_PTR as i32));
@@ -184,7 +184,7 @@ fn emit_default_new_obj_fastpath(
     let gc_cache = asm.load(Opnd::mem(64, ractor, ractor_newobj_cache_offset));
 
     let cursor = asm.load(Opnd::mem(64, gc_cache, cursor_offset));
-    let cursor_end = asm.load(Opnd::mem(64, gc_cache, jit_cursor_end_offset));
+    let cursor_end = asm.load(Opnd::mem(64, gc_cache, cursor_end_offset));
 
     let new_cursor = asm.add(cursor, Opnd::UImm(slot_size));
     asm.cmp(cursor_end, new_cursor);

gc/gc_impl.h
@@ -13,14 +13,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-struct gc_bump_pointer_heap {
-    uintptr_t cursor;
-    uintptr_t cursor_end;
-    uintptr_t jit_cursor_end;
-    uintptr_t region_start;
-    size_t    slot_size;
-};
-
 enum rb_gc_zjit_fastpath_kind {
     RB_GC_ZJIT_FASTPATH_DEFAULT = 1,
     RB_GC_ZJIT_FASTPATH_MMTK = 2,

zjit/bindgen/src/main.rs
@@ -91,7 +91,6 @@ fn main() {
         .allowlist_type("RBasic")
 
         .allowlist_type("RArray")
-        .allowlist_type("gc_bump_pointer_heap")
         .allowlist_type("rb_gc_zjit_fastpath_kind")
         .allowlist_type("rb_gc_zjit_fastpath")
         .allowlist_type("rb_gc_zjit_fastpath_data")

zjit/src/cruby_bindings.inc.rs
@@ -1968,15 +1968,6 @@ pub type jit_bindgen_constants = i32;
 pub const rb_invalid_shape_id: shape_id_t = 524287;
 pub type rb_iseq_param_keyword_struct =
     rb_iseq_constant_body_rb_iseq_parameters_rb_iseq_param_keyword;
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct gc_bump_pointer_heap {
-    pub cursor: usize,
-    pub cursor_end: usize,
-    pub jit_cursor_end: usize,
-    pub region_start: usize,
-    pub slot_size: usize,
-}
 pub const RB_GC_ZJIT_FASTPATH_DEFAULT: rb_gc_zjit_fastpath_kind = 1;
 pub const RB_GC_ZJIT_FASTPATH_MMTK: rb_gc_zjit_fastpath_kind = 2;
 pub type rb_gc_zjit_fastpath_kind = u32;
@@ -1994,7 +1985,7 @@ pub struct rb_gc_zjit_fastpath {
 #[repr(C)]
 pub struct rb_gc_zjit_default_new_obj_fastpath {
     pub cursor_offset: usize,
-    pub jit_cursor_end_offset: usize,
+    pub cursor_end_offset: usize,
     pub slot_size: usize,
     pub flags: VALUE,
     pub klass: VALUE,

commit e53c943dea406f661d9973d210a286e9f8050201

ZJIT: Fix GC bookkeeping in the default GC allocation fastpath
The inline allocation fastpath for the default GC mirrors only the
cursor bump of ractor_cache_allocate_slot(), skipping two pieces of
bookkeeping that the interpreter always performs:

* heap_cache->allocated_objects_count is not incremented, so
  total_allocated_objects undercounts objects allocated from JIT code
  and GC.verify_internal_consistency crashes with "inconsistent live
  slot number". Reproducible on a dev_nodebug build with:
  ruby --zjit-call-threshold=1 test/ruby/test_array.rb \
    -n test_replace_wb_variable_width_alloc
  Incremental marking step pacing also reads this counter.

* RUBY_INTERNAL_EVENT_NEWOBJ hooks are bypassed entirely, so
  TracePoint/ObjectSpace tools miss objects allocated from JIT code.
  Reproducible with test/-ext-/tracepoint/test_tracepoint.rb
  -n test_tracks_objspace_count. Check ruby_vm_event_flags like
  rb_gc_event_hook_required_p() and take the slow path while NEWOBJ
  hooks are enabled, which is what the MMTk fastpath already does via
  newobj_tracing_p_func.

Both regressed in 583668d5ed1 ("Remove gc_bump_pointer_heap", GH-17731).
The original fastpath from GH-17277 handled both cases: allocations
were counted from cursor movement ((cursor - region_start) / slot_size),
which included JIT inline allocations, and a dedicated jit_cursor_end
window was kept closed by gc_ractor_jit_cursor_sync() while NEWOBJ
hooks were enabled. GH-17731 switched counting to an explicit
allocated_objects_count++ that only the interpreter performs, and
removed jit_cursor_end without a replacement for the hook check.

Note that this fastpath is compiled out on RUBY_DEBUG builds because
of RACTOR_CHECK_MODE, which is why ZJIT CI (--enable-zjit=dev) does
not catch these.

gc/default/default.c
@@ -2650,6 +2650,7 @@ rb_gc_impl_zjit_new_obj_fastpath(void *objspace_ptr, size_t alloc_size, VALUE fl
     struct rb_gc_zjit_default_new_obj_fastpath default_fastpath = {
         base + offsetof(rb_ractor_newobj_heap_cache_t, cursor),
         base + offsetof(rb_ractor_newobj_heap_cache_t, cursor_end),
+        base + offsetof(rb_ractor_newobj_heap_cache_t, allocated_objects_count),
         slot_size,
         flags,
         klass

gc/default/zjit_fastpath.h
@@ -10,6 +10,7 @@
 struct rb_gc_zjit_default_new_obj_fastpath {
     size_t cursor_offset;
     size_t cursor_end_offset;
+    size_t alloc_count_offset;
     size_t slot_size;
     VALUE flags;
     VALUE klass;

zjit/src/codegen/gc_fastpath.rs
@@ -13,6 +13,7 @@ use super::JITState;
 struct RbGcZjitDefaultNewObjFastpath {
     cursor_offset: usize,
     cursor_end_offset: usize,
+    alloc_count_offset: usize,
     slot_size: usize,
     flags: VALUE,
     klass: VALUE,
@@ -61,7 +62,13 @@ unsafe extern "C" {
         klass: VALUE,
         fastpath: *mut RbGcZjitFastpath,
     ) -> bool;
+
+    /// Global event flags (rb_event_flag_t) checked by rb_gc_event_hook_required_p().
+    static ruby_vm_event_flags: u32;
 }
+
+/// RUBY_INTERNAL_EVENT_NEWOBJ in include/ruby/internal/event.h
+const RUBY_INTERNAL_EVENT_NEWOBJ: u64 = 0x100000;
 
 enum PreparedNewObjFastpath {
     Default(RbGcZjitDefaultNewObjFastpath),
@@ -177,7 +184,17 @@ fn emit_default_new_obj_fastpath(
 ) -> Option<Opnd> {
     let cursor_offset: i32 = fastpath.cursor_offset.try_into().ok()?;
     let cursor_end_offset: i32 = fastpath.cursor_end_offset.try_into().ok()?;
+    let alloc_count_offset: i32 = fastpath.alloc_count_offset.try_into().ok()?;
     let slot_size: u64 = fastpath.slot_size.try_into().ok()?;
+
+    // Take the slow path while NEWOBJ event hooks are enabled, like
+    // rb_gc_event_hook_required_p() in rb_newobj(). The slow path fires the
+    // hook for tracing (e.g. ObjectSpace) tools; the inline fast path doesn't.
+    asm_comment!(asm, "check newobj event hooks");
+    let event_flags_ptr = asm.load(Opnd::const_ptr(&raw const ruby_vm_event_flags as *const u8));
+    let event_flags = asm.load(Opnd::mem(32, event_flags_ptr, 0));
+    asm.test(event_flags, Opnd::UImm(RUBY_INTERNAL_EVENT_NEWOBJ));
+    asm.jnz(jit, miss.clone());
 
     let thread = asm.load(Opnd::mem(64, EC, RUBY_OFFSET_EC_THREAD_PTR as i32));
     let ractor = asm.load(Opnd::mem(64, thread, RUBY_OFFSET_THREAD_RACTOR as i32));
@@ -194,6 +211,15 @@ fn emit_default_new_obj_fastpath(
     asm.jl(jit, miss.clone());
 
     asm.store(Opnd::mem(64, gc_cache, cursor_offset), new_cursor);
+
+    // Count the allocation like ractor_cache_allocate_slot() so that
+    // total_allocated_objects and incremental marking step pacing stay
+    // accurate. GC.verify_internal_consistency reports "inconsistent live
+    // slot number" when this count is missed.
+    let alloc_count = asm.load(Opnd::mem(64, gc_cache, alloc_count_offset));
+    let new_alloc_count = asm.add(alloc_count, Opnd::UImm(1));
+    asm.store(Opnd::mem(64, gc_cache, alloc_count_offset), new_alloc_count);
+
     asm.store(
         Opnd::mem(VALUE_BITS, cursor, RUBY_OFFSET_RBASIC_FLAGS),
         fastpath.flags.as_u64().into(),